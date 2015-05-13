aba254254936ebc51967a62d74cbd8da2cf659f8
Matthew Lawrence-Stubley <quakematt@outlook.com>
2015-05-13 23:41:50 +0100

    Define the public API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
 'use strict';
+
+// Include dependencies
+var gulp = require('gulp');
+
+// Export Gelf public API
+module.exports = {
+	
+	/**
+	 * Define a task.
+	 */
+	task: gulp.task.bind(gulp),
+	
+	
+	/**
+	 * Watch files and directories for changes.
+	 */
+	watch: gulp.watch.bind(gulp),
+	
+	
+	/**
+	 * Emit files matching provided glob or an array of globs.
+	 */
+	src: gulp.src.bind(gulp),
+	
+	
+	/**
+	 * Write a file or files to a destination.
+	 */
+	dest: gulp.dest.bind(gulp),
+	
+	
+	/**
+	 * Start a task.
+	 */
+	start: gulp.start.bind(gulp),
+	
+	
+	/**
+	 * Configure a Gelf task.
+	 */
+	config: function(name, config) {
+		
+	},
+	
+	
+	/**
+	 * Load Gelf tasks from a file, directory, or object.
+	 */
+	load: function(target) {
+		
+	}
+	
+};
