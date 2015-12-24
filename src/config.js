5fd99887cc184fb4265ef1310cd7d1e0c861b436
Matthew Lawrence-Stubley <quakematt@outlook.com>
2015-12-24 16:02:38 +0000

    Add support for merging CLI arguments into configuration

--- src/config.js
+++ src/config.js
@@ -5,11 +5,94 @@ var lib = {
 	extend: require('extend'),
 };
 
-// var argv = require('minimist')(process.argv.slice(2));
+
+// Utilities
+// ---------------------------------------------------------
+
+/**
+ * Execute a function on all leaves of an object graph.
+ */
+function recursiveMap(value, callback) {
+
+	if (Array.isArray(value)) {
+		return value.map(callback);
+	}
+
+	if (typeof value === 'object' || typeof value === 'function') {
+		for (var k in value) {
+			value[k] = recursiveMap(value[k], callback);
+		}
+		return value;
+	}
+
+	return callback(value);
+
+}
 
 
 // Private API
 // ---------------------------------------------------------
+
+/**
+ * Parse CLI arguments into an object.
+ */
+function parseArgs() {
+
+	var out = {};
+	var argv = require('minimist')(process.argv.slice(2));
+
+	for (var k in argv) {
+		if (k !== '_') {
+			out[k] = argv[k];
+		}
+	}
+
+	return recursiveMap(out, function(v) {
+
+		if (v === 'true') {
+			return true;
+		}
+
+		if (v === 'false') {
+			return false;
+		}
+
+		if (v === 'null') {
+			return null;
+		}
+
+		if (v === 'undefined') {
+			return undefined;
+		}
+
+		return v;
+
+	});
+
+}
+
+
+/**
+ * Get CLI arguments for the named configuration.
+ */
+function getArgs(name) {
+
+	if (getArgs.parsed == null) {
+		getArgs.parsed = parseArgs();
+	}
+
+	// Signature: getArgs()
+	//   Get all args
+	if (arguments.length === 0) {
+		return getAllConfig(gelf);
+	}
+
+	// Signature: getArgs(name)
+	//   Get args for the named config
+	return getArgs.parsed[name] || {};
+
+}
+
 
 /**
  * Get a named configuration objects.
@@ -20,7 +103,9 @@ function getConfig(gelf, name) {
 		? getConfig(gelf, 'global')
 		: {};
 
-	return (gelf._config[name] || []).reduce(function(config, current) {
+	var configurators = (gelf._config[name] || []).concat([getArgs(name)]);
+
+	return configurators.reduce(function(config, current) {
 
 		if (current == null) {
 			return {};
